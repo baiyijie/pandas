06836f7991a44dae82d206376195cd139233b247
baiyijie <17801182239@163.com>
2026-03-30 10:38:04 +0800

    Update version.py

--- pandas/version.py
+++ pandas/version.py
@@ -1,3 +1,20 @@
+import sys
+import os
+from pathlib import Path
+
+# 1. 获取当前脚本的绝对路径
+current_file = Path(__file__).resolve()
+
+# 2. 获取项目的根目录 (当前文件的上一级目录的上一级)
+# .parent 是 folder_a, .parent.parent 就是 project_root
+project_root = current_file.parent
+
+# 3. 将项目根目录添加到搜索路径中
+if str(project_root) not in sys.path:
+    sys.path.insert(0, str(project_root))
+# 测试调用
+# data_tool.your_function()
+
 from compat import qq
 
 # Make sure to modify __release_datetime__ to release time when making official release.
